e6098a4acb63116d560c2f71dbeb64979ef4343c
Eloisa Fagundes <lola.fernanda@hotmail.com>
2020-08-27 21:41:22 -0300

    adiciona pasta components

---
+++ challenge-provi/src/components/Appbar/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Toolbar } from "@material-ui/core";
+import { AppBar } from "@material-ui/core";
+
+
+function Appbar() {
+
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Appbar;
